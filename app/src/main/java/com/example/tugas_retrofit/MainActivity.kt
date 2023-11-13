b6562b6104ac15fb9a135fb1c8b3c68bf42d9027
Harits <haritsirawan2118@gmail.com>
2023-11-13 20:22:14 +0700

    first

---
+++ app/src/main/java/com/example/tugas_retrofit/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tugas_retrofit
+
+import android.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tugas_retrofit.databinding.ActivityMainBinding
+import com.example.tugas_retrofit.model.Users
+import com.example.tugas_retrofit.network.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val client = ApiClient.getInstance()
+        val response = client.getAllUsers()
+        val userList = ArrayList<String>()
+        response.enqueue(object : Callback<Users> {
+            override fun onResponse(call: Call<Users>, response: Response<Users>) {
+                for (i in response.body()!!.data) {
+                    userList.add(i.title)
+                }
+                val listAdapter = ArrayAdapter(
+                    this@MainActivity,
+                    R.layout.simple_list_item_1,
+                    userList
+                )
+                with(binding){
+                    recycle.apply {
+                        adapter = Adapter(response.body()!!.data)
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        val gridLayoutManager = GridLayoutManager(context, 2)
+                        layoutManager = gridLayoutManager
+                    }
+                }
+            }
+            override fun onFailure(call: Call<Users>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Koneksi error",
+                    Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
